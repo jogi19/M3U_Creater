b4004430e20fea80fffc8e551800eb539e7cd7dd
Jochen Weiss <jochen.weiss@mail.de>
2018-02-05 21:53:27 +0100

    removed some pylint findings

--- create_m3u.py
+++ create_m3u.py
@@ -19,40 +19,41 @@ def check_dir(direc):
     @param direc directory where the search starts
     '''
     found_m3u = False
-    foundMusicFiles = []
+    found_music_files = []
     for filename in os.listdir(direc):
         if((filename.find('.mp3') > 0) or (filename.find('.MP3') > 0)):
-            foundMusicFiles.append(filename)
+            found_music_files.append(filename)
             print ('MP3: '+filename)
         elif((filename.find('.ogg') > 0) or (filename.find('.OGG') > 0)):
-            foundMusicFiles.append(filename)
+            found_music_files.append(filename)
             print ('OGG: '+filename)
         elif(filename.find('.m3u') > 0 or filename.find('.M3U')):
             found_m3u = True
             print ('found_m3u: '+ filename)
         print (filename)
 
-    foundMusicFiles.sort()
-    if (not found_m3u and (len(foundMusicFiles) > 0)):
+    found_music_files.sort()
+    l_fmf = len(found_music_files)
+    if (not found_m3u and l_fmf > 0):
         print('###################################')
-        a = ""
+        a_help = ""
         if platform.system().find('Linux') > -1:
-            a = direc.rfind('/')
+            a_help = direc.rfind('/')
         else:
-            a = direc.rfind('\\') # on Windows
-        directoryName = direc[a+1:]
-        print(directoryName)
-        fi = open(direc+"/"+directoryName+".m3u", "w")
+            a_help = direc.rfind('\\') # on Windows
+        directory_name = direc[a_help+1:]
+        print(directory_name)
+        fil = open(direc+"/"+directory_name+".m3u", "w")
 
 
-        for music in foundMusicFiles:
+        for music in found_music_files:
             print(music)
-            fi.write(music+'\n')
-        fi.close()
+            fil.write(music+'\n')
+        fil.close()
 
-startDir = sys.argv[1]
+START_DIR = sys.argv[1]
 
-for dirname, dirnames, filenames in os.walk(startDir):
+for dirname, dirnames, filenames in os.walk(START_DIR):
     for subdirname in dirnames:
         print ("FOUND DIRECTORY: ", os.path.join(dirname, subdirname))
         check_dir(os.path.join(dirname, subdirname))
